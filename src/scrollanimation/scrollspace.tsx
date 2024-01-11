dc4dd90263eefcedc99c64859f73347f3c8cae30
Matthew Jamison <matthewjamison56@gmail.com>
2024-01-10 19:00:56 -0800

    new

--- src/scrollanimation/scrollspace.tsx
+++ src/scrollanimation/scrollspace.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from 'react';
 
 const ScrollSpaceComponent = forwardRef<HTMLDivElement>((_, ref) => {
   return (
-    <div ref={ref} style={{height: '175vh'}}></div> // Apply forwarded ref here
+    <div ref={ref} style={{height: '200vh'}}></div> // Apply forwarded ref here
   );
 });
 
